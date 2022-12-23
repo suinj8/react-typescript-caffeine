d3dc057e803a70bc438bc533f19da95f2596b068
JSI <suin8@jbnu.ac.kr>
2022-12-23 20:06:39 +0900

    feat: add footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
+import Footer from "../components/Footer";
+
 const Home = () => {
-  return <div>HOME화면</div>;
+  return (
+    <div>
+      HOME화면
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
