5f2acce0c09ba8179cc486c4c9377fcbf427ae7e
JSI <suin8@jbnu.ac.kr>
2022-12-25 17:57:19 +0900

    feat: update ICamp interface

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,8 +1,8 @@
 export interface ICamp {
   ID: number;
-  status: "모집중" | "모집완료" | "모집전";
-  jobs: string;
-  field: string;
+  status?: "모집중" | "모집완료" | "모집전";
+  jobs?: string;
+  field?: string;
   title: string;
   sDate: string;
 }
