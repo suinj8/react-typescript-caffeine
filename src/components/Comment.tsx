1351a57c747f6a6caba92e793b20885d474bda9e
JSI <suin8@jbnu.ac.kr>
2022-12-31 00:14:50 +0900

    design: replace css location, change className

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,38 +3,37 @@ import fonts from "../styles/fonts";
 
 import commentProfile from "../assets/images/commentProfile.png";
 
-const Container = styled.div<{ picture: string | undefined }>`
-  display: flex;
+const Comment = ({ ...props }) => {
+  return (
+    <CommentBox picture={props.picture}>
+      <div className="comment-picture"></div>
 
-  .picture {
+      <div className="comment-contentBox">
+        <div className="comment-nickname">{props.nickname}</div>
+        <div className="comment-content">{props.content}</div>
+      </div>
+    </CommentBox>
+  );
+};
+
+const CommentBox = styled.div<{ picture: string | undefined }>`
+  display: flex;
+  .comment-picture {
     background-image: ${(props) =>
       props.picture ? `url(${props.picture})` : `url(${commentProfile})`};
     width: 30px;
     height: 30px;
     margin-right: 8px;
   }
-  .contentBox {
-    .nickname {
+  .comment-contentBox {
+    .comment-nickname {
       ${fonts.Caption};
       font-weight: bold;
     }
-    .content {
+    .comment-content {
       ${fonts.Caption}
     }
   }
 `;
-
-const Comment = ({ ...props }) => {
-  return (
-    <Container picture={props.picture}>
-      <div className="picture"></div>
-
-      <div className="contentBox">
-        <div className="nickname">{props.nickname}</div>
-        <div className="content">{props.content}</div>
-      </div>
-    </Container>
-  );
-};
 
 export default Comment;
